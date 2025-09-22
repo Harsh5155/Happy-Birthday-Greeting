273b59696233713351d4ed098f49948b0aafdc73
Harsh5155 <htalekar15@gmail.com>
2025-09-22 22:44:14 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './index.css';
 
+// Pass props directly to ensure the message is visible
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <App 
+      name="Anjali"
+      message="Happy Birthday Anjali !!! You're an amazing person, and I'm so lucky to know you. Wishing you a day filled with joy, laughter, and everything you wished for. Cheers to another fantastic year! 💖 Many more birthdays to celebrate together Stay Blessed Our childhood friendship have grown into a Treasure HAPPY 20 th"
+      imageUrl="/birthday_img1.jpg"
+    />
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
