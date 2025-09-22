1daa87422dd4c59e491388211bc07fcfa7511f48
Harsh5155 <htalekar15@gmail.com>
2025-09-22 23:06:35 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,15 +3,16 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 
-// Pass props directly to ensure the message is visible
+// Change this to your JPEG file in public folder
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App 
       name="Anjali"
       message="Happy Birthday Anjali !!! You're an amazing person, and I'm so lucky to know you. Wishing you a day filled with joy, laughter, and everything you wished for. Cheers to another fantastic year! 💖 Many more birthdays to celebrate together Stay Blessed Our childhood friendship have grown into a Treasure HAPPY 20 th"
-      imageUrl="/birthday_img1.jpg"
+      imageUrl="/my_photo.jpeg" // <-- Put your jpeg in public folder
     />
   </React.StrictMode>
 );
+
 
