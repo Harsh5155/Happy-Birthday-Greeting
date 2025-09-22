13e63d039b2d2eea7d4a5e6cdebdc38eae05f7dc
Harsh5155 <htalekar15@gmail.com>
2025-09-22 23:14:48 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -9,17 +9,18 @@ root.render(
   <React.StrictMode>
     <App 
       name="Anjali"
-      message="🎉🎂 Happy Birthday Anjali!!! 🎂🎉
+      message={`🎉🎂 Happy Birthday Anjali!!! 🎂🎉
 You're an amazing person 🌟, and I'm so lucky to know you 🤗.
 Wishing you a day filled with joy 😄, laughter , and everything you wished for ✨.
 Cheers to another fantastic year! 🥳💖
 Many more birthdays to celebrate together 🥂🎈
 Stay Blessed 🙏💫
 Our childhood friendship has grown into a Treasure 💛
-HAPPY 20th 🎊🎁🎉"
+HAPPY 20th Birthday I Hope you loved your birthday gift ..... 🎊🎁🎉`}
       imageUrl="/my_photo.jpeg" // <-- Put your jpeg in public folder
     />
   </React.StrictMode>
 );
+
 
 
